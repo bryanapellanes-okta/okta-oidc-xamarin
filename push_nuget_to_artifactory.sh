cb569d759c0e7079aed3e024a931bfe86257a2bc
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-08-12 09:19:29 -0700

    add cake build tasks to pipeline

--- push_nuget_to_artifactory.sh
+++ push_nuget_to_artifactory.sh
@@ -4,10 +4,8 @@ if [ -d "./artifacts/Common" ]; then
     for NUGETPACKAGE in $(ls ./artifacts/Common/*.nupkg)
     do
         echo "Pushing ${NUGETPACKAGE} to OKTA_NUGET_TOPIC_REPO"
-        echo "${ARTIFACTORY_NUGET_APIKEY}"
         dotnet nuget push ${NUGETPACKAGE} -k ${ARTIFACTORY_NUGET_APIKEY} -s OKTA_NUGET_TOPIC_REPO
     done
 else
-    echo "${ARTIFACTORY_NUGET_APIKEY}"
     echo "./artifacts/Common Was not found; no post build work needed.";
 fi
