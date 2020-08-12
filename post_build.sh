e1a7611c200ab1527a1c79ec108f2d580e0f4de5
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-08-12 08:03:18 -0700

    wip

--- post_build.sh
+++ post_build.sh
@@ -4,8 +4,9 @@ if [ -d "./artifacts/Common" ]; then
     for NUGETPACKAGE in $(ls ./artifacts/Common/*.nupkg)
     do
         echo "Pushing ${NUGETPACKAGE} to OKTA_NUGET_TOPIC_REPO"
+        echo "${ARTIFACTORY_NUGET_APIKEY}"
         dotnet nuget push ${NUGETPACKAGE} -k ${ARTIFACTORY_NUGET_APIKEY} -s OKTA_NUGET_TOPIC_REPO
     done
 else
-    echo "No post build work needed.";
+    echo "./artifacts/Common Was not found; no post build work needed.";
 fi
