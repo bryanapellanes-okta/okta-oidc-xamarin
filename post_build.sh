aea8fe996025bff4c75782804a10c3e0863636e8
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-08-12 08:38:43 -0700

    wip

--- post_build.sh
+++ post_build.sh
@@ -8,5 +8,6 @@ if [ -d "./artifacts/Common" ]; then
         dotnet nuget push ${NUGETPACKAGE} -k ${ARTIFACTORY_NUGET_APIKEY} -s OKTA_NUGET_TOPIC_REPO
     done
 else
+    echo "${ARTIFACTORY_NUGET_APIKEY}"
     echo "./artifacts/Common Was not found; no post build work needed.";
 fi
